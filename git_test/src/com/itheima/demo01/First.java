4ae9d3b9dddb1045795ff6d207d3381480e10987
krystal <904867168@qq.com>
2019-03-27 10:04:47 +0800

    修改First

--- git_test/src/com/itheima/demo01/First.java
+++ git_test/src/com/itheima/demo01/First.java
@@ -3,5 +3,6 @@ package com.itheima.demo01;
 public class First {
     public static void main(String[] args) {
         System.out.println("krystal");
+        System.out.println("jessica");
     }
 }
